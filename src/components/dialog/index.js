abb3ecf02389268e75806e0eac96f6c134ea5d41
Jantwel <pahutv@weblium.com>
2017-09-04 19:04:10 +0300

    fetch repo info refactor

--- src/components/dialog/index.js
+++ src/components/dialog/index.js
@@ -2,6 +2,8 @@ import { h, Component } from 'preact'
 import request from '../../services/request'
 import css from './style.css'
 
+const getData = url => fetch(url).then(response => response.json())
+
 export default class Dialog extends Component {
   constructor() {
     super()
@@ -15,22 +17,22 @@ export default class Dialog extends Component {
   closeDialog = () => this.props.closeRepo(this.props.matches.name)
   closeByEscape = () => event.key === 'Escape' && this.closeDialog()
 
-  getUrls = repo => [
-    repo.url + '/contributors?per_page=3',
-    repo.languages_url,
-    repo.url + '/pulls?&sort=popularity&direction=desc&per_page=5'
-  ]
+  getUrls = repo =>
+    [
+      repo.url + '/contributors?per_page=3',
+      repo.languages_url,
+      repo.url + '/pulls?&sort=popularity&direction=desc&per_page=5'
+    ].map(getData)
 
   fetchRepo = async () => {
-    const { matches: { name, repoName } } = this.props
-    const { body: repo } = await request(
-      `//api.github.com/repos/${name}/${repoName}`
-    )
-    const [contributors, languages, pulls] = await request.all(
-      this.getUrls(repo)
-    )
-
-    this.setState({ loading: false, repo, contributors, languages, pulls })
+    // const { matches: { name, repoName } } = this.props
+    // const { body: repo } = await request(
+    //   `//api.github.com/repos/${name}/${repoName}`
+    // )
+    //
+    // Promise.all(this.getUrls(repo)).then(([contributors, languages, pulls]) =>
+    //   this.setState({ loading: false, repo, contributors, languages, pulls })
+    // )
   }
 
   componentWillMount() {
@@ -44,41 +46,44 @@ export default class Dialog extends Component {
 
   render({}, { loading, repo, contributors, pulls }) {
     return (
-      !loading &&
       <div class={css.dialogWrapper}>
         <div class={css.backcover} onClick={this.closeDialog} />
         <div class={css.dialog}>
           <button class={css.closeBtn} onClick={this.closeDialog}>
             X
           </button>
-          <h3 class={css.header}>
-            {repo && repo.full_name}
-          </h3>
-          <div>
-            <h4>Contributors</h4>
-            {contributors &&
-              contributors.map(({ login, url, contributions }) =>
-                <div>
-                  <a href={url}>
-                    {login}
-                  </a>
-                  <span>
-                    Contributions: {contributions}
-                  </span>
-                </div>
-              )}
-          </div>
-          <div>
-            <h4>Pull Requests</h4>
-            {pulls &&
-              pulls.map(({ title, url }) =>
-                <div>
-                  <a href={url}>
-                    {title}
-                  </a>
-                </div>
-              )}
-          </div>
+          {loading
+            ? <div>LOADING...</div>
+            : <div>
+              <h3 class={css.header}>
+                {repo && repo.full_name}
+              </h3>
+              <div>
+                <h4>Contributors</h4>
+                {contributors &&
+                    contributors.map(({ login, url, contributions }) =>
+                      <div>
+                        <a href={url}>
+                          {login}
+                        </a>
+                        <span>
+                          Contributions: {contributions}
+                        </span>
+                      </div>
+                    )}
+              </div>
+              <div>
+                <h4>Pull Requests</h4>
+                {pulls &&
+                    pulls.map(({ title, url }) =>
+                      <div>
+                        <a href={url}>
+                          {title}
+                        </a>
+                      </div>
+                    )}
+              </div>
+            </div>}
         </div>
       </div>
     )
--- src/services/request.js
+++ src/services/request.js
@@ -12,10 +12,4 @@ const request = (url, options = {}) =>
     .then(response => Promise.all([response.json(), response.headers]))
     .then(configureData)
 
-request.all = urls =>
-  urls.map(async url => {
-    const { body } = await request(url)
-    return body
-  })
-
 export default request
