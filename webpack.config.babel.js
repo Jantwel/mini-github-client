1a8f14d08c6949e179c8a7f33785cc6c6f2039ad
Jantwel <pahutv@weblium.com>
2017-09-05 11:31:06 +0300

    add auto deploy

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,185 +6,186 @@ import CopyWebpackPlugin from 'copy-webpack-plugin'
 import OfflinePlugin from 'offline-plugin'
 import path from 'path'
 const ENV = process.env.NODE_ENV || 'development'
+const PUBLIC_PATH = ENV === 'development' ? '/' : '/mini-github-client/'
 
 const CSS_MAPS = ENV !== 'production'
 
 module.exports = {
-	context: path.resolve(__dirname, 'src'),
-	entry: ['babel-polyfill', './index.js'],
-	output: {
-		path: path.resolve(__dirname, 'docs'),
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
+  context: path.resolve(__dirname, 'src'),
+  entry: ['babel-polyfill', './index.js'],
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: PUBLIC_PATH,
+    filename: 'bundle.js'
+  },
 
-	resolve: {
-		extensions: ['.jsx', '.js', '.json', '.sass', '.scss'],
-		modules: [
-			path.resolve(__dirname, 'src/lib'),
-			path.resolve(__dirname, 'node_modules'),
-			'node_modules'
-		],
-		alias: {
-			components: path.resolve(__dirname, 'src/components'), // used for tests
-			style: path.resolve(__dirname, 'src/style'),
-			react: 'preact-compat',
-			'react-dom': 'preact-compat'
-		}
-	},
+  resolve: {
+    extensions: ['.jsx', '.js', '.json', '.sass', '.scss'],
+    modules: [
+      path.resolve(__dirname, 'src/lib'),
+      path.resolve(__dirname, 'node_modules'),
+      'node_modules'
+    ],
+    alias: {
+      components: path.resolve(__dirname, 'src/components'), // used for tests
+      style: path.resolve(__dirname, 'src/style'),
+      react: 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				exclude: path.resolve(__dirname, 'src'),
-				enforce: 'pre',
-				use: 'source-map-loader'
-			},
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				use: 'babel-loader'
-			},
-			{
-				test: /\.(scss|sass|css)$/,
-				include: [path.resolve(__dirname, 'src/components')],
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						{
-							loader: 'css-loader',
-							options: { modules: true, sourceMap: CSS_MAPS, importLoaders: 1 }
-						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: true
-							}
-						},
-						{
-							loader: 'postcss-loader',
-							options: {
-								sourceMap: CSS_MAPS,
-								plugins: () => {
-									autoprefixer({ browsers: ['last 2 versions'] })
-								}
-							}
-						}
-					]
-				})
-			},
-			{
-				test: /\.json$/,
-				use: 'json-loader'
-			},
-			{
-				test: /\.(xml|html|txt|md)$/,
-				use: 'raw-loader'
-			},
-			{
-				test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
-				use: ENV === 'production' ? 'file-loader' : 'url-loader'
-			}
-		]
-	},
-	plugins: [
-		new webpack.NoEmitOnErrorsPlugin(),
-		new ExtractTextPlugin({
-			filename: 'style.css',
-			allChunks: true,
-			disable: ENV !== 'production'
-		}),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(ENV)
-		}),
-		new HtmlWebpackPlugin({
-			template: './index.ejs',
-			minify: { collapseWhitespace: true }
-		}),
-		new CopyWebpackPlugin([
-			{ from: './manifest.json', to: './' },
-			{ from: './assets/favicon.ico', to: './' }
-		]),
-		new OfflinePlugin({
-			relativePaths: false,
-			AppCache: false,
-			excludes: ['_redirects'],
-			ServiceWorker: {
-				events: true
-			},
-			cacheMaps: [
-				{
-					match: /.*/,
-					to: '/',
-					requestTypes: ['navigate']
-				}
-			],
-			publicPath: '/'
-		})
-	].concat(
-		ENV === 'production'
-			? [
-				new webpack.optimize.UglifyJsPlugin({
-					output: {
-						comments: false
-					},
-					compress: {
-						unsafe_comps: true,
-						properties: true,
-						keep_fargs: false,
-						pure_getters: true,
-						collapse_vars: true,
-						unsafe: true,
-						warnings: false,
-						screw_ie8: true,
-						sequences: true,
-						dead_code: true,
-						drop_debugger: true,
-						comparisons: true,
-						conditionals: true,
-						evaluate: true,
-						booleans: true,
-						loops: true,
-						unused: true,
-						hoist_funs: true,
-						if_return: true,
-						join_vars: true,
-						cascade: true,
-						drop_console: true
-					}
-				})
-			]
-			: []
-	),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: path.resolve(__dirname, 'src'),
+        enforce: 'pre',
+        use: 'source-map-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        include: [path.resolve(__dirname, 'src/components')],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: { modules: true, sourceMap: CSS_MAPS, importLoaders: 1 }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: CSS_MAPS,
+                plugins: () => {
+                  autoprefixer({ browsers: ['last 2 versions'] })
+                }
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
+      },
+      {
+        test: /\.(xml|html|txt|md)$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.(svg|woff2?|ttf|eot|jpe?g|png|gif)(\?.*)?$/i,
+        use: ENV === 'production' ? 'file-loader' : 'url-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true,
+      disable: ENV !== 'production'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(ENV)
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.ejs',
+      minify: { collapseWhitespace: true }
+    }),
+    new CopyWebpackPlugin([
+      { from: './manifest.json', to: './' },
+      { from: './assets/favicon.ico', to: './' }
+    ]),
+    new OfflinePlugin({
+      relativePaths: false,
+      AppCache: false,
+      excludes: ['_redirects'],
+      ServiceWorker: {
+        events: true
+      },
+      cacheMaps: [
+        {
+          match: /.*/,
+          to: '/',
+          requestTypes: ['navigate']
+        }
+      ],
+      publicPath: PUBLIC_PATH
+    })
+  ].concat(
+    ENV === 'production'
+      ? [
+        new webpack.optimize.UglifyJsPlugin({
+          output: {
+            comments: false
+          },
+          compress: {
+            unsafe_comps: true,
+            properties: true,
+            keep_fargs: false,
+            pure_getters: true,
+            collapse_vars: true,
+            unsafe: true,
+            warnings: false,
+            screw_ie8: true,
+            sequences: true,
+            dead_code: true,
+            drop_debugger: true,
+            comparisons: true,
+            conditionals: true,
+            evaluate: true,
+            booleans: true,
+            loops: true,
+            unused: true,
+            hoist_funs: true,
+            if_return: true,
+            join_vars: true,
+            cascade: true,
+            drop_console: true
+          }
+        })
+      ]
+      : []
+  ),
 
-	stats: { colors: true },
+  stats: { colors: true },
 
-	node: {
-		global: true,
-		process: false,
-		Buffer: false,
-		__filename: false,
-		__dirname: false,
-		setImmediate: false
-	},
+  node: {
+    global: true,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false,
+    setImmediate: false
+  },
 
-	devtool: ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
+  devtool: ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
 
-	devServer: {
-		port: process.env.PORT || 8050,
-		host: 'localhost',
-		publicPath: '/',
-		contentBase: './src',
-		historyApiFallback: true,
-		open: true,
-		openPage: '',
-		proxy: {
-			// OPTIONAL: proxy configuration:
-			// '/optional-prefix/**': { // path pattern to rewrite
-			//   target: 'http://target-host.com',
-			//   pathRewrite: path => path.replace(/^\/[^\/]+\//, '')   // strip first path segment
-			// }
-		}
-	}
+  devServer: {
+    port: process.env.PORT || 8050,
+    host: 'localhost',
+    publicPath: '/',
+    contentBase: './src',
+    historyApiFallback: true,
+    open: true,
+    openPage: '',
+    proxy: {
+      // OPTIONAL: proxy configuration:
+      // '/optional-prefix/**': { // path pattern to rewrite
+      //   target: 'http://target-host.com',
+      //   pathRewrite: path => path.replace(/^\/[^\/]+\//, '')   // strip first path segment
+      // }
+    }
+  }
 }
